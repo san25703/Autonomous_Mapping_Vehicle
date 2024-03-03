d4abb79eec5096269d3b46bd8ee393e97e98b96a
san25703 <102456642+san25703@users.noreply.github.com>
2024-03-03 10:48:30 +0530

    Update Encoder_Algorithm.ino

--- Encoder_Algorithm.ino
+++ Encoder_Algorithm.ino
@@ -1,59 +1,64 @@
 #include <Encoder.h>
 
+// Define the pins for the rotary encoder
 #define EN_A 2
 #define EN_B 7
 
+// Create an Encoder object
 Encoder Enc(EN_A, EN_B);
 
+// Variables for time and encoder position tracking
 long previousMillis = 0;
 long currentMillis = 0;
-
 volatile long currentEncoder;
 volatile long previousEncoder = 0;
-volatile long oldPosition = 0;
-volatile long newPosition;
 volatile long diff;
 
+// Variables for main position and rotational speed tracking
 long positionMain  = -999;
-float old_rot_speed=0;
+float old_rot_speed = 0;
 
 void setup() 
 {
-  // put your setup code here, to run once:
-  Serial.begin(9600); //Set the band rate to your Bluetooth module.
+  // Initialize serial communication and set up pins
+  Serial.begin(9600); // Set the baud rate for communication
   pinMode(EN_A, INPUT_PULLUP);
-  pinMode(EN_B,  INPUT_PULLUP); 
+  pinMode(EN_B, INPUT_PULLUP); 
   Serial.println("TwoKnobs Encoder Test:");
 }
 
 float read_speed(void)
 {
-    //read velocity of selected motor
-    //return velocity in rad/s
-    const int Encoder_1_round = 4000; //define number of pulses in one round of encoder
+    // Read the velocity of the selected motor
+    // Return velocity in rad/s
+    const int Encoder_1_round = 4000; // Define the number of pulses in one round of the encoder
     currentEncoder = Enc.read();
-    diff=currentEncoder-previousEncoder;
+    diff = currentEncoder - previousEncoder;
     
-    float rot_speed;           //rotating speed in rad/s
-    const int interval = 1000; //choose interval is 1 second (1000 milliseconds)
+    float rot_speed;           // Rotating speed in rad/s
+    const int interval = 1000; // Choose the interval as 1 second (1000 milliseconds)
     currentMillis = millis();
 
     if (currentMillis - previousMillis > interval)
     {
         previousMillis = currentMillis;
-        rot_speed = (float)(diff*60/ (Encoder_1_round));
+        rot_speed = (float)(diff * 60 / (Encoder_1_round));
         previousEncoder = currentEncoder;
         return rot_speed;
     }
 }
 
-void loop() {
-float new_rot_speed;
-new_rot_speed=read_speed();
-if (new_rot_speed != old_rot_speed) {
-Serial.print("Speed = ");
-Serial.print(new_rot_speed);
-Serial.println();
-old_rot_speed = new_rot_speed;
+void loop() 
+{
+    float new_rot_speed;
+    new_rot_speed = read_speed();
+    
+    // Print the speed if it has changed
+    if (new_rot_speed != old_rot_speed) {
+        Serial.print("Speed = ");
+        Serial.print(new_rot_speed);
+        Serial.println();
+        old_rot_speed = new_rot_speed;
+    }
 }
-}
+```
